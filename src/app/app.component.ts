90bf4a2f6e40add0e7463c88103e3ee4dacc3fab
jess88tw <jess88tw@yahoo.com.tw>
2023-02-23 18:45:41 +0800

    modify Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,32 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'HelloWorld';
+  // text interpolations
+  title = 'Todos';
+
+  // property binding
+  placeholder = 'Whats needs to be done???';
+
+  toggleAllBtn = false;
+  check1 = false;
+  check2 = false;
+
+  toggleAll() {
+    this.toggleAllBtn = !this.toggleAllBtn;
+    this.check1 = this.toggleAllBtn;
+    this.check2 = this.toggleAllBtn;
+  }
+  // event binding
+  toggleOne(event: MouseEvent) {
+    // 傳入事件後可以抓值
+    console.log(event);
+    this.check1 = !this.check1;
+  }
+
+  toggleTwo() {
+    this.check2 = !this.check2;
+  }
 }
